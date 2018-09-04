9aabeb64472da3602146e7edb9e822c5e7caa64f
ws752499660 <ws752499660@foxmail.com>
2018-09-04 23:28:40 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,21 @@ const app = getApp()
 Page({
   data: {
     swiperImg:[
-      'http://suo.im/4Sc8zX',
-      'http://suo.im/5fvlKD',
-      'http://suo.im/57eyOv'
+      {
+        id:1,
+        url: 'https://i.loli.net/2018/09/04/5b8ea02cf23d2.png',
+        title:"我是第一篇文章的标题"
+      },
+      {
+        id:2,
+        url: 'https://i.loli.net/2018/09/04/5b8ea02cc22b6.jpg',
+        title: "我是第二篇文章的标题"
+      },
+      {
+        id:3,
+        url: 'https://i.loli.net/2018/09/04/5b8ea02cf1d45.jpg',
+        title: "我是第三篇文章的标题"
+      },
     ]
   },
 
@@ -24,6 +36,12 @@ Page({
       url: './mustknow/mustknow',
       success: function (res) { },
       fail: function (res) { },
+    })
+  },
+
+  onItemClick: function(e){
+    wx.navigateTo({
+      url: './imgArticle/imgArticle?id='+e.target.id,
     })
   }
 })
--- pages/me/me.js
+++ pages/me/me.js
@@ -22,18 +22,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(app.appData.loginflag)
-    if(!app.appData.loginflag)
-    {
-      wx.redirectTo({
-        url: './login/login',
-      })
-    }
-    else
-    {
-      this.setData({id:app.appData.user.id})
-    }
-  
   },
 
   /**
